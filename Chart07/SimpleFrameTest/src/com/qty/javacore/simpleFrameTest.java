f99d8ffdc8f244df8f2be254c729bea8e73ca3a8
xiaotuan <583168425@qq.com>
2017-01-31 13:47:03 +0800

    添加Chart07源代码

---
+++ Chart07/SimpleFrameTest/src/com/qty/javacore/simpleFrameTest.java
@@ -0,0 +1,22 @@
+package com.qty.javacore;
+
+import javax.swing.JFrame;
+
+public class simpleFrameTest {
+
+	public static void main(String[] args) {
+		SimpleFrame frame = new SimpleFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
+
+class SimpleFrame extends JFrame {
+	
+	public SimpleFrame() {
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+	
+	public static final int DEFAULT_WIDTH = 300;
+	public static final int DEFAULT_HEIGHT = 200;
+}
---
+++ Chart07/SizedFrameTest/src/com/qty/javacore/SizedFrameTest.java
@@ -0,0 +1,45 @@
+package com.qty.javacore;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class SizedFrameTest {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				SizedFrame frame = new SizedFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+}
+
+
+class SizedFrame extends JFrame {
+	
+	public SizedFrame() {
+		// get screen dimensions
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		
+		// set frame width, height and let platform pick screen location
+		
+		setSize(screenWidth / 2, screenHeight / 2);
+		setLocationByPlatform(true);
+		
+		// set frame icon and title
+		
+		Image img = kit.getImage("icon.png");
+		setIconImage(img);
+		setTitle("SizeFrame");
+	}
+}
