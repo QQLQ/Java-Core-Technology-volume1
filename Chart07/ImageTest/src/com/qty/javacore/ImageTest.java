32acbbfb2b4cfb15987295c413ff1c892ed81d03
xiaotuan <583168425@qq.com>
2017-02-01 16:45:39 +0800

    更新第七章源代码

---
+++ Chart07/ImageTest/src/com/qty/javacore/ImageTest.java
@@ -0,0 +1,81 @@
+package com.qty.javacore;
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class ImageTest {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				ImageFrame frame = new ImageFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+}
+
+/**
+ * A frame with an image component
+ */
+class ImageFrame extends JFrame {
+	
+	public ImageFrame() {
+		setTitle("ImageTest");
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		
+		// add component to frame
+		ImageComponent component = new ImageComponent();
+		add(component);
+	}
+	
+	public static final int DEFAULT_WIDTH = 300;
+	public static final int DEFAULT_HEIGHT = 200;
+}
+
+
+/**
+ * A component that displays a tiled image
+ */
+class ImageComponent extends JComponent {
+	
+	public ImageComponent() {
+		// acquire the image 
+		try {
+			image = ImageIO.read(new File("blue-ball.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void paintComponent(Graphics g) {
+		if (image == null) {
+			return;
+		}
+		
+		int imageWidth = image.getWidth(this);
+		int imageHeight = image.getHeight(this);
+		
+		// draw the image in the top-left corner
+		g.drawImage(image, 0, 0, null);
+		// tile the image across the component
+		for (int i = 0; i * imageWidth <= getWidth(); i++) {
+			for (int j = 0; j * imageHeight <= getHeight(); j++) {
+				if (i + j > 0) {
+					g.copyArea(0,  0,  imageWidth, imageHeight, i * imageWidth, j * imageHeight);
+				}
+			}
+		}
+	}
+	
+	private Image image;
+}
